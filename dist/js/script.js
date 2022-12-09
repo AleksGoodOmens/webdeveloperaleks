37546a824c8a5c3a1e5ae41a0fc7be674332d040
Aleksei <agsfinks@gmail.com>
2022-12-09 10:54:19 +0000

    adaptive

--- dist/js/script.js
+++ dist/js/script.js
@@ -21,6 +21,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
+    const progresBarValue = document.querySelectorAll('.progress-bar__percentage'),
+        progressBarFiller = document.querySelectorAll('.progress-bar__filler');
+    progresBarValue.forEach((percentage, index) => {
+        progressBarFiller[index].style.width = percentage.textContent;
+    });
+    // [data-short] in html to cut text
+    const largeText = document.querySelectorAll('[data-short]');
+    largeText.forEach(element => {
+
+        textShorter(element);
+    });
 
     //functions
     function addClass(element, className) {
@@ -29,4 +40,27 @@ document.addEventListener('DOMContentLoaded', () => {
     function removeClass(element, className) {
         element.classList.remove(className);
     }
+
+    function textShorter(element, textLenght) {
+        textLenght = element.getAttribute('data-short');
+        if (textLenght == '') {
+            textLenght = 50;
+        }
+        if (element.textContent.length > textLenght) {
+            element.textContent = element.textContent.slice(0, textLenght) + '...';
+        }
+    }
+
+    const smoothLinks = document.querySelectorAll('a[href^="#"]');
+    for (let smoothLink of smoothLinks) {
+        smoothLink.addEventListener('click', function (e) {
+            e.preventDefault();
+            const id = smoothLink.getAttribute('href');
+
+            document.querySelector(id).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    };
 });
