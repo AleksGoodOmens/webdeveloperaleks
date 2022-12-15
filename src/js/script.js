201f66c39a4ae180725e6dc0f208cd8e6c02154f
Aleksei <agsfinks@gmail.com>
2022-12-15 11:03:33 +0000

    version 1.00

--- src/js/script.js
+++ src/js/script.js
@@ -21,9 +21,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-    const progresBarValue = document.querySelectorAll('.progress-bar__percentage'),
+    const progressBarValue = document.querySelectorAll('.progress-bar__percentage'),
         progressBarFiller = document.querySelectorAll('.progress-bar__filler');
-    progresBarValue.forEach((percentage, index) => {
+
+    progressBarValue.forEach((percentage, index) => {
         progressBarFiller[index].style.width = percentage.textContent;
     });
     // [data-short] in html to cut text
@@ -41,19 +42,19 @@ document.addEventListener('DOMContentLoaded', () => {
         element.classList.remove(className);
     }
 
-    function textShorter(element, textLenght) {
-        textLenght = element.getAttribute('data-short');
-        if (textLenght == '') {
-            textLenght = 50;
+    function textShorter(element, textLength) {
+        textLength = element.getAttribute('data-short');
+        if (textLength == '') {
+            textLength = 50;
         }
-        if (element.textContent.length > textLenght) {
-            element.textContent = element.textContent.slice(0, textLenght) + '...';
+        if (element.textContent.length > textLength) {
+            element.textContent = element.textContent.slice(0, textLength) + '...';
         }
     }
 
     const smoothLinks = document.querySelectorAll('a[href^="#"]');
     for (let smoothLink of smoothLinks) {
-        smoothLink.addEventListener('click', function (e) {
+        smoothLink.addEventListener('click', (e) => {
             e.preventDefault();
             const id = smoothLink.getAttribute('href');
 
@@ -62,5 +63,153 @@ document.addEventListener('DOMContentLoaded', () => {
                 block: 'start'
             });
         });
-    };
+    }
+
+    // const file = document.querySelector('input[type="file"]');
+    // const remover = document.createElement('div');
+    // const label = file.nextElementSibling;
+
+    // inputFile();
+    function inputFile() {
+
+        file.addEventListener('change', (e) => {
+
+            if (file.files.length != 0) {
+                if (file.files.length == 1) {
+                    let indexOfName = 1;
+                    for (let i in file.value) {
+                        if (file.value[i] == '\\') {
+                            indexOfName = i;
+                        }
+                    }
+                    indexOfName++;
+                    label.lastChild.textContent = file.value.slice(indexOfName);
+                    textShorter(label.lastChild);
+
+                } else if (file.files.length > 1 && file.files.length < 5) {
+                    label.lastChild.textContent = `${file.files.length} - файла выбрано`;
+
+                } else {
+                    label.lastChild.textContent = `${file.files.length} - файлов выбрано`;
+                }
+                remover.className = 'remover';
+                remover.innerHTML = '<div>X</div>';
+                label.parentElement.append(remover);
+                remover.addEventListener('click', () => {
+                    file.value = '';
+                    label.lastChild.textContent = 'Добавить файлы';
+                    remover.remove();
+                });
+            }
+            if (file.files.length == 0) {
+                remover.className = 'remover';
+                remover.innerHTML = '<div>X</div>';
+                label.parentElement.append(remover);
+                remover.addEventListener('click', () => {
+                    file.value = '';
+                    label.textContent = 'Добавить файлы';
+                    remover.remove();
+                });
+            }
+        });
+    }
+    function resetFileInput() {
+        file.value = '';
+        label.lastChild.textContent = 'Добавить файлы';
+        remover.remove();
+
+    }
+
+
+    //! forms
+
+    // const validate = new window.JustValidate('#form', {
+    //     errorLabelStyle: {
+    //     }
+    // });
+    // validate
+    //     .addField('#name', [
+    //         {
+    //             rule: 'minLength',
+    //             value: 3,
+    //         },
+    //         {
+    //             rule: 'maxLength',
+    //             value: 30,
+    //         },
+    //     ])
+    //     .addField('#email', [
+    //         {
+    //             rule: 'required',
+    //             errorMessage: 'Укажите Email',
+    //         },
+    //         {
+    //             rule: 'email',
+    //             errorMessage: 'Возможно вы допустили ошибку!',
+    //         },
+    //     ])
+    //     .addField('#message', [
+    //         {
+    //             rule: 'required',
+    //             errorMessage: 'Пожалуйста напишите хотя бы пару слов!',
+    //         },
+    //         {
+    //             rule: 'minLength',
+    //             value: 5,
+    //             errorMessage: 'Сообщение должно быть более 5 символов',
+    //         },
+    //     ])
+    //     .addField('#policy', [
+    //         {
+    //             rule: 'required',
+    //             errorMessage: 'Обязательно поставьте галочку, после ознакомления с политикой конфиденциальности',
+    //         }
+    //     ]).onSuccess((event) => {
+    //         let data = new FormData(event.target);
+    //         let xhr = new XMLHttpRequest();
+    //         const submBtn = event.target.querySelector('#submit-btn');
+    //         submBtn.textContent = 'Отправляется...';
+    //         submBtn.style.background = 'green';
+    //         xhr.onreadystatechange = function () {
+    //             if (xhr.readyState === 4) {
+    //                 if (xhr.status === 200) {
+    //                     console.log('Validation passes and form submitted');
+    //                     submBtn.textContent = 'Спасибо!';
+    //                     submBtn.style.background = 'green';
+    //                     remover.remove();
+
+    //                 }
+    //             }
+    //         };
+
+    //         xhr.open('POST', 'mailer/smart.php', true);
+    //         xhr.send(data);
+    //         resetFileInput();
+    //         event.target.reset();
+
+    //     });
+
+    //! scroll Animations
+    AOS.init(
+        { // Global settings:
+            disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+            startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+            initClassName: 'aos-init', // class applied after initialization
+            animatedClassName: 'aos-animate', // class applied on animation
+            useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+            disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+            debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+            throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+            // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+            offset: 120, // offset (in px) from the original trigger point
+            delay: 0, // values from 0 to 3000, with step 50ms
+            duration: 400, // values from 0 to 3000, with step 50ms
+            easing: 'ease', // default easing for AOS animations
+            once: true, // whether animation should happen only once - while scrolling down
+            mirror: false, // whether elements should animate out while scrolling past them
+            anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation;
+        }
+    );
 });
