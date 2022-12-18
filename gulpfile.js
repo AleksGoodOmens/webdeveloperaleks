4bcb9a5b7d1764faefa4763577153640302d596d
Aleks G <agsfinks@gmail.com>
2022-12-18 13:08:23 +0000

    version 1.001

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 'use strict';
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
 const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
+
 
 gulp.task('server', function () {
     browserSync({
